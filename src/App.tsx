eecdfba77744b0abce438f947f5a8431125e74ae
Anna <a.khripchenko@student.inha.uz>
2025-04-22 14:51:15 +0500

    add footer, change header color and links hover

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@
 import { BrowserRouter } from 'react-router-dom'
 import Header from './components/header'
 import Home from './pages/home'
+import Footer from './components/footer'
 function App() {
 
   return (
     <BrowserRouter>
       <Header/>
       <Home/>
+      <Footer/>
     </BrowserRouter>
   )
 }
