b4a27c84763e4c8b062ff575060168504a680180
Maksud Jumaniyozov <m9989323954@gmail.com>
2025-04-29 18:42:15 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true,  // слушает на 0.0.0.0 для внешнего доступа
+    port: 5173,  // внутренний порт в контейнере
+    strictPort: true,
+  },
+});
